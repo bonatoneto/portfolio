6ecdf4152f296fc1c285b2461aaed8ff90c52818
bonatoneto <joaoadolfobonato@gmail.com>
2023-02-02 03:13:33 -0300

    .
    .

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    /*
+
+    if (html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }
+
+    OU 
+
+    html.classList.toggle('light')
+
+    */
+
+    html.classList.toggle('light')
+
+    //trocando o avatar 
+    const img = document.querySelector("#profile img")
+    if (html.classList.contains('lights')) {
+        img.setAttribute('src', './assets/Avatar-lightmode.png')
+    } else {
+        img.setAttribute('src', './assets/Avatar-darkmode.png')
+    }
+}
